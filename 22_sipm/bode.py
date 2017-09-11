853a289db1d6613bcf6ce0b4146b2f1f06972b38
pfackeldey <peter.fackeldey@rwth-aachen.de>
2017-09-11 15:17:03 +0200

    plotting = done

--- 22_sipm/bode.py
+++ 22_sipm/bode.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+def func(x, c):
+     return c
 
 with open ("bode/sipm__fe_ch1.txt","r") as channel1:
     lines=channel1.readlines()
     result=[]
     for line in lines:
         result.append(line.strip().split('\t'))
-    result_cropped = result[11:-1]
+    result_cropped = result[11::]
     frequency= []
     log_mag = []
     phase = []
-    print result_cropped[1][1]
     for i in range(len(result_cropped)):
-        frequency[i] = result_cropped[i][1]
-        log_mag[i] = result_cropped[i][2]
-        phase[i] = result_cropped[i][3]
+        frequency.append(result_cropped[i][1])
+        log_mag.append(result_cropped[i][2])
+        phase.append(result_cropped[i][3])
+
+    ax1 = plt.subplot(211)
+    plt.semilogx(frequency, log_mag)
+    plt.grid(True)
+    ax2 = plt.subplot(212, sharex=ax1)
+    plt.plot(frequency, phase)
+    plt.grid(True)
+    plt.show()
