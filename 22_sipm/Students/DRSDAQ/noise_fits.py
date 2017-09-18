2025177305322425a85e8a68a17a8a1348190946
Sebastian Wuchterl <sebastian.wuchterl@rwth-aachen.de>
2017-09-18 10:15:08 +0200

    changed minor stuff for fits

--- 22_sipm/Students/DRSDAQ/noise_fits.py
+++ 22_sipm/Students/DRSDAQ/noise_fits.py
@@ -18,6 +18,7 @@ parser = argparse.ArgumentParser(description="Perform topmass plots.",
 args = parser.parse_args()
 
 
+<<<<<<< 0b14836e4a2ac9eb3df8ed2d5011d8d80544fc10
 data_samples = ['root-scripts/finger574.root', 'root-scripts/finger564.root',
                 'root-scripts/finger554.root', 'root-scripts/finger544.root', 'root-scripts/finger534.root']
 OVs = ['finger574', 'finger564', 'finger554', 'finger544', 'finger534']
@@ -27,6 +28,16 @@ fitranges = [[[17., 28.], [38., 44.], [55., 65.]],
              [[10.5, 17.5], [22., 30.], [35., 45.]],
              [[10.5, 21.], [22., 38.]],
              [[13.5, 18.5], [13.5, 18.5]]]
+=======
+data_samples = ['root-scripts/finger574.root','root-scripts/finger564.root','root-scripts/finger554.root','root-scripts/finger544.root','root-scripts/finger534.root']
+OVs = ['finger574','finger564','finger554','finger544','finger534']
+Title = ['574','564','554','544','534']
+fitranges = [[[17.,28.],[38.,44.],[55.,65.]],
+			[[11.,18.75],[26.,38.],[44.,52.]],
+			[[10.5,17.5],[22.,30.],[35.,45.]],
+			[[10.5,21.],[22.,38.]],
+			[[13.5,18.5],[13.5,18.5]]]
+>>>>>>> changed minor stuff for fits
 
 arProb = []
 arErrProb = []
@@ -73,6 +84,7 @@ for i in xrange(5):
     #~ legend.AddEntry(data, 'Data', 'p')
     #~ legend.AddEntry(ttbar, 'TTbar', 'f')
 
+<<<<<<< 0b14836e4a2ac9eb3df8ed2d5011d8d80544fc10
     # fit
     def func(x, par):
         ret = par[0] * 2.718281**(-0.5 * ((x[0] - par[1]) / par[2])**2.)
@@ -98,6 +110,38 @@ for i in xrange(5):
         data.Fit('g2', "+", "", fitranges[i][1][0], fitranges[i][1][1])
     #~ if (i<2):
         #~ data.Fit('g3',"+","",fitranges[i][2][0],fitranges[i][2][1])
+=======
+    #fit
+    def func(x,par):
+		ret=par[0]*2.718281**(-0.5*((x[0]-par[1])/par[2])**2.)
+		#~ un=par[3]
+		#~ return ret+un
+		return ret
+
+    #~ gaus1 = ROOT.TF1('g1', func, fitranges[i][0][0],fitranges[i][0][1], 4)
+    gaus1 = ROOT.TF1('g1', func, fitranges[i][0][0],fitranges[i][0][1], 3)
+    gaus1.SetParNames("amplitude","mean","width")
+    gaus1.SetParameters(22000.,20.,5.)
+    
+
+    
+    if(i<3):
+		#~ gaus2 = ROOT.TF1('g2', func, fitranges[i][1][0],fitranges[i][1][1], 4)
+		gaus2 = ROOT.TF1('g2', func, fitranges[i][1][0],fitranges[i][1][1], 3)
+		gaus2.SetParNames("amplitude","mean","width")
+		gaus2.SetParameters(2200.,20.,5.)
+		
+    if(i<2):
+		gaus3 = ROOT.TF1('g3', func, fitranges[i][2][0],fitranges[i][2][1], 3)
+		gaus3.SetParNames("amplitude","mean","width")
+		gaus3.SetParameters(200.,20.,5.)	
+
+    data.Fit('g1',"","",fitranges[i][0][0],fitranges[i][0][1])
+    if (i<3):
+		data.Fit('g2',"+","",fitranges[i][1][0],fitranges[i][1][1])
+    if (i<2):
+		data.Fit('g3',"+","",fitranges[i][2][0],fitranges[i][2][1])
+>>>>>>> changed minor stuff for fits
     #~ ROOT.gStyle.SetOptFit(1)
 
     # fit parameter to legend
@@ -116,14 +160,16 @@ for i in xrange(5):
 # calculate probability:
 
     #~ border1=[30,24,20,20,20]
-    border1 = [30, 22, 21, 22, 28]
-    er1_nom = ROOT.Double(0)
-    er1_denom = ROOT.Double(0)
-    nominator = data.IntegralAndError(border1[i], 90, er1_nom)
-    denominator = data.IntegralAndError(0, 90, er1_denom)
-    prob = nominator / denominator
-    er1 = (er1_nom / nominator)**2. + (er1_denom / denominator)**2.
-    er1 = np.sqrt(er1) * prob
+
+    border1=[30,22,20,22,20]
+    er1_nom=ROOT.Double(0)
+    er1_denom=ROOT.Double(0)
+    nominator=data.IntegralAndError(border1[i],90,er1_nom)
+    denominator=data.IntegralAndError(0,90,er1_denom)
+    prob=nominator/denominator
+    er1=(er1_nom/nominator)**2.+(er1_denom/denominator)**2.
+    er1=np.sqrt(er1)*prob
+
     arProb.append(prob)
     arErrProb.append(er1)
     print "prob1", prob * 100., "+-", er1 * 100.
@@ -150,8 +196,8 @@ arErrP = np.array(arErrProb)
 arV = np.array([5., 4., 3., 2., 1.])
 print arV
 plt.figure(1)
-plt.errorbar(arV, arP, yerr=arErrP)
+plt.errorbar(arV,arP*100.,yerr=arErrP*100.,fmt="o-")
 plt.xlabel("OV [V]")
-plt.ylabel("crosstalk probability")
-plt.grid()
+plt.ylabel("crosstalk probability [\%]")
+#~ plt.grid()
 plt.savefig("test.pdf")
