82dd721b90166cb0b23af4dc3c12f3b1cd0f5044
Sebastian Wuchterl <sebastian.wuchterl@rwth-aachen.de>
2017-09-27 15:36:19 +0200

    calibration nearly done

--- 23_Muon/calibration/cali.py
+++ 23_Muon/calibration/cali.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from scipy import stats
 
 dataMean=[]
 dataErr=[]
@@ -15,41 +16,37 @@ for i in range(1,46):
     lengthMax=length
     x=np.linspace(0,length,length)
     plt.figure()
-    plt.yscale("log")
-    # plt.plot(x,temp,".")
+    # plt.yscale("log")
     plt.xlabel("channel number")
     plt.ylabel("N")
     plt.title("")
     plt.grid()
-
     tempMax=np.max(temp)
     tempMaxIndex=np.argmax(temp)
 
-    # print temp
-
-    # tempMean=np.average(x,weights=temp)
-    # tempStd=np.sqrt(np.average((x-tempMean)**2.,weights=temp))
-    # tempErr=tempStd/np.sqrt(sum(temp))
-    # print tempMean,"+-",tempErr
-    # plt.axvline(tempMean,0.,tempMax,color='red',alpha=0.5)
 
     if (i==1):
-        plt.plot(x[0:tempMaxIndex+100],temp[0:tempMaxIndex+100],".")
-        tempMean=np.average(x[0:tempMaxIndex+100],weights=temp[0:tempMaxIndex+100])
-        tempStd=np.sqrt(np.average((x[0:tempMaxIndex+100]-tempMean)**2.,weights=temp[0:tempMaxIndex+100]))
-        tempErr=tempStd/np.sqrt(sum(temp[0:tempMaxIndex+100]))
+        bin_means, bin_edges, binnumber = stats.binned_statistic(x[0:tempMaxIndex+100],temp[0:tempMaxIndex+100],statistic='mean', bins=100)
     else:
         if(i==45):
-            plt.plot(x[tempMaxIndex-100:],temp[tempMaxIndex-100:],".")
-            tempMean=np.average(x[tempMaxIndex-100:],weights=temp[tempMaxIndex-100:])
-            tempStd=np.sqrt(np.average((x[tempMaxIndex-100:]-tempMean)**2.,weights=temp[tempMaxIndex-100:]))
-            tempErr=tempStd/np.sqrt(sum(temp[tempMaxIndex-100:]))
+            bin_means, bin_edges, binnumber = stats.binned_statistic(x[tempMaxIndex-100:],temp[tempMaxIndex-100:],statistic='mean', bins=100)
         else:
-            plt.plot(x[tempMaxIndex-100:tempMaxIndex+100],temp[tempMaxIndex-100:tempMaxIndex+100],".")
-            tempMean=np.average(x[tempMaxIndex-100:tempMaxIndex+100],weights=temp[tempMaxIndex-100:tempMaxIndex+100])
-            tempStd=np.sqrt(np.average((x[tempMaxIndex-100:tempMaxIndex+100]-tempMean)**2.,weights=temp[tempMaxIndex-100:tempMaxIndex+100]))
-            tempErr=tempStd/np.sqrt(sum(temp[tempMaxIndex-100:tempMaxIndex+100]))
-    plt.axvline(tempMean,0.,tempMax,color='red',alpha=0.5)
+
+            bin_means, bin_edges, binnumber = stats.binned_statistic(x[tempMaxIndex-100:tempMaxIndex+100],temp[tempMaxIndex-100:tempMaxIndex+100],
+                statistic='mean', bins=100)
+            # plt.plot(x[tempMaxIndex-100:tempMaxIndex+100],temp[tempMaxIndex-100:tempMaxIndex+100],".")
+            # tempMean=np.average(x[tempMaxIndex-100:tempMaxIndex+100],weights=temp[tempMaxIndex-100:tempMaxIndex+100])
+            # tempStd=np.sqrt(np.average((x[tempMaxIndex-100:tempMaxIndex+100]-tempMean)**2.,weights=temp[tempMaxIndex-100:tempMaxIndex+100]))
+            # tempErr=tempStd/np.sqrt(sum(temp[tempMaxIndex-100:tempMaxIndex+100]))
+    plt.hlines(bin_means, bin_edges[:-1], bin_edges[1:], colors='g', lw=2)
+    binCenter = bin_edges[:-1]+(bin_edges[1:]-bin_edges[:-1])/2.
+    tempMean=np.average(binCenter,weights=bin_means)
+    tempStd=np.sqrt(np.average((binCenter-tempMean)**2.,weights=bin_means))
+    tempErr=tempStd/np.sqrt(sum(binCenter))
+
+    plt.axvline(tempMean+tempErr,0.,tempMax,color='orange',alpha=0.2,lw=0.5)
+    plt.axvline(tempMean-tempErr,0.,tempMax,color='orange',alpha=0.2,lw=0.5)
+    plt.axvline(tempMean,0.,tempMax,color='red',alpha=0.8,lw=0.5)
     dataMean.append(tempMean)
     dataErr.append(tempErr)
     plt.savefig("time_channels/"+str(i)+".pdf")
@@ -61,18 +58,15 @@ def func(x,a,b):
 
 
 times=np.array(times)
-times=times+0.015
+# times=times+0.015
 erTimes=np.array(erTimes)
-# erTimes=np.array(erTimes)/np.sqrt(12.)*2.
+erTimes=np.array(erTimes)/np.sqrt(12.)*2.
 dataMean=np.array(dataMean)
 dataErr=np.array(dataErr)
 
-# times=np.delete(times,[1,3,5])
-# erTimes=np.delete(erTimes,[1,3,5])
-# dataMean=np.delete(dataMean,[1,3,5])
-# dataErr=np.delete(dataErr,[1,3,5])
 
 popt,pcov=curve_fit(func,dataMean,times)
+# popt,pcov=curve_fit(func,dataMean,times,sigma=erTimes)
 print popt[0],"+-",np.sqrt(pcov[0][0])
 print popt[1],"+-",np.sqrt(pcov[1][1])
 
@@ -91,14 +85,19 @@ print chi2(y_fit,times,errNewY)/(len(erTimes)-2.)
 fig = plt.figure(1)
 fig.suptitle("Time Calibration")
 ax1 = fig.add_subplot(211)
-ax1.errorbar(dataMean,times,xerr=dataErr,yerr=erTimes,fmt=".",linewidth=0.5,capsize=1.5,markersize=2.5)
+ax1.errorbar(dataMean,times,xerr=dataErr,yerr=erTimes,fmt=".",linewidth=0.5,capsize=1.5,markersize=2.5,label="data")
 ax1.grid(True)
 ax1.set_title("Measurement")
 plt.ylabel("$\Delta$ time [$\mu$ s]")
 plt.xlabel("Channel Number")
-plt.plot(x_linspace,y_linspace)
+plt.plot(x_linspace,y_linspace,label="fit")
+
+ax1.text(10000, 4, 'a= '+str(round(popt[0]*1000.,4))+" $\pm$ "+str(round(np.sqrt(pcov[0][0])*1000.,4))+"\nb= "+str(round(popt[1]*1000.,2))+" $\pm$ "+str(round(np.sqrt(pcov[1][1])*1000.,2)), style='italic',fontsize="small",
+        bbox={'facecolor':'grey', 'alpha':0.6, 'pad':5})
+plt.legend(loc="best")
 ax2 = fig.add_subplot(212, sharex=ax1)
 ax2.set_title("Resdiues")
+
 
 # y_res=times-func(times,popt[0],popt[1])
 ax2.errorbar(dataMean,y_res,xerr=dataErr,yerr=errNewY,fmt=".",linewidth=0.5,capsize=1.5,markersize=2.5)
