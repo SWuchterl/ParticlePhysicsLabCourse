c719afdc0346c7d649e8c022f88bc89e7529f06a
Sebastian Wuchterl <sebastian.wuchterl@rwth-aachen.de>
2017-10-08 12:11:36 +0200

    minor bug fixed

--- 23_Muon/calibration/systematic_fits.py
+++ 23_Muon/calibration/systematic_fits.py
@@ -1,18 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-ar_A=[]
-ar_B=[]
-ar_erA=[]
-ar_erB=[]
-ar_A_without=[]
-ar_B_without=[]
-ar_erA_without=[]
-ar_erB_without=[]
-ar_i=[]
+ar_A = []
+ar_B = []
+ar_erA = []
+ar_erB = []
+ar_A_without = []
+ar_B_without = []
+ar_erA_without = []
+ar_erB_without = []
+ar_i = []
 
-for i in range(10,200,10):
-    temp=np.loadtxt("values/cali"+str(i)+".txt",skiprows=0)
+for i in range(10, 200, 10):
+    temp = np.loadtxt("values/cali" + str(i) + ".txt", skiprows=0)
     ar_A.append(temp[0])
     ar_B.append(temp[2])
     ar_erA.append(np.sqrt(temp[1]))
@@ -26,13 +26,13 @@ for i in range(10,200,10):
 fig = plt.figure()
 ax1 = fig.add_subplot(211)
 ax1.set_title("slope a")
-ax1.errorbar(ar_i,ar_A,yerr=ar_erA,fmt=".")
-ax1.errorbar(ar_i,ar_A_without,yerr=ar_erA_without,fmt=".")
+ax1.errorbar(ar_i, ar_A, yerr=ar_erA, fmt=".")
+# ax1.errorbar(ar_i,ar_A_without,yerr=ar_erA_without,fmt=".")
 plt.xlabel("n Bins")
 ax2 = fig.add_subplot(212, sharex=ax1)
 ax2.set_title("intercept b")
-ax2.errorbar(ar_i,ar_B,yerr=ar_erB,fmt=".")
-ax2.errorbar(ar_i,ar_B_without,yerr=ar_erB_without,fmt=".")
+ax2.errorbar(ar_i, ar_B, yerr=ar_erB, fmt=".")
+# ax2.errorbar(ar_i, ar_B_without, yerr=ar_erB_without, fmt=".")
 plt.xlabel("n Bins")
 plt.subplots_adjust(hspace=0.5, wspace=1.0)
 # plt.show()
